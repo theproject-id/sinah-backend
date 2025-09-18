cf89fda7219de960c6c752fe3bbc48266fdcfff2
Lucas Josino <contact@lucasjosino.com>
2025-09-17 21:39:34 -0300

    feat: add initial files

---
+++ src/main/java/br/com/sinah/SinahApplication.java
@@ -0,0 +1,13 @@
+package br.com.sinah;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SinahApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SinahApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/sinah/SinahApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.sinah;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SinahApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
