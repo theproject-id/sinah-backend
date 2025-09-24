bd4f5ef69cfe8d144521a854491064a19c0af569
Lucas Josino <contact@lucasjosino.com>
2025-09-23 22:51:33 -0300

    feat: add security config

---
+++ src/main/java/br/com/sinah/common/config/SecurityConfig.java
@@ -0,0 +1,84 @@
+package br.com.sinah.common.config;
+
+import br.com.sinah.user.service.UserService;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.ProviderManager;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.List;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Value("${br.com.sinah.application.webapp-origins}")
+    private String allowedOrigin;
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http.csrf(AbstractHttpConfigurer::disable)
+                .cors(cors -> cors
+                        .configurationSource(corsConfigurationSource())
+                )
+                .authorizeHttpRequests(authorize -> authorize
+                        // Options HTTP
+                        .requestMatchers(HttpMethod.OPTIONS, "/**").permitAll()
+
+                        // Public routes
+                        .requestMatchers(HttpMethod.GET, "/api/v1/ping").permitAll()
+
+                        // Auth routes
+                        .requestMatchers(
+                                "/api/v1/auth/login",
+                                "/api/v1/auth/me",
+                                "/api/v1/auth/logout"
+                        ).permitAll()
+
+                        // User routes (admin only)
+                        .requestMatchers("/api/v1/users/**").hasRole("ADMIN")
+
+                        .anyRequest().denyAll()
+                )
+                .build();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(
+            UserService userService,
+            PasswordEncoder passwordEncoder
+    ) {
+        DaoAuthenticationProvider provider = new DaoAuthenticationProvider(userService);
+        provider.setPasswordEncoder(passwordEncoder);
+        return new ProviderManager(provider);
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowedHeaders(List.of("Content-Type", "Authorization"));
+        config.setAllowedOrigins(List.of(allowedOrigin));
+        config.setAllowedMethods(List.of("GET", "POST", "PATCH", "DELETE", "OPTIONS"));
+        config.setExposedHeaders(List.of("Set-Cookie"));
+        source.registerCorsConfiguration("/**", config);
+        return source;
+    }
+}
