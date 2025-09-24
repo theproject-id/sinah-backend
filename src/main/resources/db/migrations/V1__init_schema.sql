abd3f01d7d1dc38f61d1c1359bb26331ec924f99
Lucas Josino <contact@lucasjosino.com>
2025-09-23 22:48:45 -0300

    feat: add refresh_tokens table

--- src/main/resources/db/migrations/V1__init_schema.sql
+++ src/main/resources/db/migrations/V1__init_schema.sql
@@ -1,6 +1,6 @@
 CREATE TABLE users (
-        id              INTEGER GENERATED ALWAYS AS IDENTITY,
-        uuid UUID       PRIMARY KEY UNIQUE NOT NULL,
+        id              BIGINT GENERATED ALWAYS AS IDENTITY,
+        uuid            UUID PRIMARY KEY UNIQUE NOT NULL,
         first_name      VARCHAR(30) NOT NULL,
         last_name       VARCHAR(30) NOT NULL,
         username        VARCHAR(30) NOT NULL,
@@ -9,4 +9,17 @@ CREATE TABLE users (
         role            VARCHAR(30) NOT NULL,
         created_at      TIMESTAMP NOT NULL DEFAULT now(),
         updated_at      TIMESTAMP NOT NULL DEFAULT now()
+);
+
+CREATE TABLE refresh_tokens (
+    id              BIGINT GENERATED ALWAYS AS IDENTITY,
+    uuid            UUID PRIMARY KEY UNIQUE NOT NULL,
+    token           VARCHAR(255) NOT NULL UNIQUE,
+    user_uuid       UUID NOT NULL,
+    issued_at       TIMESTAMP NOT NULL DEFAULT NOW(),
+    expires_at      TIMESTAMP NOT NULL,
+    revoked         BOOLEAN NOT NULL DEFAULT FALSE,
+    CONSTRAINT fk_refresh_tokens_user_uuid
+        FOREIGN KEY (user_uuid) REFERENCES users(uuid)
+        ON DELETE CASCADE
 );
