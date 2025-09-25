ab0136fd84794de5aa88448370a80ada8a3f7f43
Lucas Josino <contact@lucasjosino.com>
2025-09-24 22:22:20 -0300

    feat: add /me endpoint

--- src/main/java/br/com/sinah/auth/controller/AuthController.java
+++ src/main/java/br/com/sinah/auth/controller/AuthController.java
@@ -3,13 +3,11 @@ package br.com.sinah.auth.controller;
 import br.com.sinah.auth.dto.LoginRequestDTO;
 import br.com.sinah.auth.dto.LoginResponseDTO;
 import br.com.sinah.auth.service.AuthService;
+import br.com.sinah.user.dto.UserDTO;
 import jakarta.validation.Valid;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/v1/auth")
@@ -28,5 +26,13 @@ public class AuthController {
     )
     public ResponseEntity<LoginResponseDTO> login(@RequestBody @Valid LoginRequestDTO loginRequestDTO) {
         return ResponseEntity.ok(authService.login(loginRequestDTO));
+    }
+
+    @GetMapping(
+            value = "/me",
+            produces = MediaType.APPLICATION_JSON_VALUE
+    )
+    public ResponseEntity<UserDTO> me() {
+        return ResponseEntity.ok(authService.authenticate());
     }
 }
--- src/main/java/br/com/sinah/auth/service/AuthService.java
+++ src/main/java/br/com/sinah/auth/service/AuthService.java
@@ -3,9 +3,15 @@ package br.com.sinah.auth.service;
 import br.com.sinah.auth.dto.LoginRequestDTO;
 import br.com.sinah.auth.dto.LoginResponseDTO;
 import br.com.sinah.common.jwt.JwtManager;
+import br.com.sinah.user.dto.UserDTO;
+import br.com.sinah.user.mapper.UserMapper;
 import br.com.sinah.user.model.UserModel;
+import br.com.sinah.user.service.UserService;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.oauth2.jwt.Jwt;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationToken;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -13,16 +19,20 @@ public class AuthService {
 
     private final JwtManager jwtManager;
 
+    private final UserService userService;
+
     private final RefreshTokenService refreshTokenService;
 
     private final AuthenticationManager authenticationManager;
 
     public AuthService(
             JwtManager jwtManager,
+            UserService userService,
             RefreshTokenService refreshTokenService,
             AuthenticationManager authenticationManager
     ) {
         this.jwtManager = jwtManager;
+        this.userService = userService;
         this.refreshTokenService = refreshTokenService;
         this.authenticationManager = authenticationManager;
     }
@@ -35,5 +45,13 @@ public class AuthService {
         var refreshToken = refreshTokenService.generateToken(user.getUuid());
         var jwt = jwtManager.generateToken(user, refreshToken.uuid());
         return new LoginResponseDTO(jwt, refreshToken.token(), refreshToken.expiresAt());
+    }
+
+    public UserDTO authenticate() {
+        var authenticationToken = (JwtAuthenticationToken) SecurityContextHolder.getContext().getAuthentication();
+        var credentials = (Jwt) authenticationToken.getCredentials();
+        
+        var user = userService.loadUserByUsername(credentials.getClaim("email"));
+        return UserMapper.toDTO(user);
     }
 }
