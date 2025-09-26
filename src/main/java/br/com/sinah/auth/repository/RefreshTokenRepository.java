77b5b2bc9886dfbca7230d509062c473d333834a
Lucas Josino <contact@lucasjosino.com>
2025-09-26 08:53:51 -0300

    feat: add refreshToken logic

--- src/main/java/br/com/sinah/auth/repository/RefreshTokenRepository.java
+++ src/main/java/br/com/sinah/auth/repository/RefreshTokenRepository.java
@@ -4,8 +4,10 @@ import br.com.sinah.auth.model.RefreshTokenModel;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
 import java.util.UUID;
 
 @Repository
 public interface RefreshTokenRepository extends JpaRepository<RefreshTokenModel, UUID> {
+    Optional<RefreshTokenModel> findByToken(String token);
 }
