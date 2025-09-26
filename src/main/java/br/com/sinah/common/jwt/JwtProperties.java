9c69a51a506a92581cf376f403d151ce6c9b6544
Lucas Josino <contact@lucasjosino.com>
2025-09-26 08:50:48 -0300

    feat: add JWT_REFRESH_TOKEN_TTL and JWT_ACCESS_TOKEN_TTL

--- src/main/java/br/com/sinah/common/jwt/JwtProperties.java
+++ src/main/java/br/com/sinah/common/jwt/JwtProperties.java
@@ -3,6 +3,8 @@ package br.com.sinah.common.jwt;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
 
+import java.time.Duration;
+
 @Component
 @ConfigurationProperties(prefix = "jwt")
 public class JwtProperties {
@@ -10,6 +12,10 @@ public class JwtProperties {
     private String issuerValidator;
 
     private String audienceValidator;
+
+    private Duration accessTokenTtl;
+
+    private Duration refreshTokenTtl;
 
     public String getIssuerValidator() {
         return issuerValidator;
@@ -25,5 +31,21 @@ public class JwtProperties {
 
     public void setAudienceValidator(String audienceValidator) {
         this.audienceValidator = audienceValidator;
+    }
+
+    public Duration getAccessTokenTtl() {
+        return accessTokenTtl;
+    }
+
+    public void setAccessTokenTtl(Duration accessTokenTtl) {
+        this.accessTokenTtl = accessTokenTtl;
+    }
+
+    public Duration getRefreshTokenTtl() {
+        return refreshTokenTtl;
+    }
+
+    public void setRefreshTokenTtl(Duration refreshTokenTtl) {
+        this.refreshTokenTtl = refreshTokenTtl;
     }
 }
