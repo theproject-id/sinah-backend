176e939094007d2513795c16296a580c6fde084f
Joseulisses065 <joseulissesdasilva.ads@gamil.com>
2025-09-26 20:36:00 -0300

    feat: Add swagger and lombook

---
+++ src/main/java/br/com/sinah/common/config/SecurityConfig.java
@@ -0,0 +1,26 @@
+package br.com.sinah.common.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http.csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(authorize -> authorize
+                        // Options HTTP
+                        .requestMatchers("/**").permitAll())
+                .build();
+    }
+
+
+}
