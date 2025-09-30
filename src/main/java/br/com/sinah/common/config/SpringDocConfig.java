eb6224066e0bb87005a54c9224b3f3990c801aa5
Lucas Josino <contact@lucasjosino.com>
2025-09-29 22:24:00 -0300

    feat: add bearer token to spring doc

--- src/main/java/br/com/sinah/common/config/SpringDocConfig.java
+++ src/main/java/br/com/sinah/common/config/SpringDocConfig.java
@@ -1,8 +1,11 @@
 package br.com.sinah.common.config;
 
+import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -16,6 +19,15 @@ public class SpringDocConfig {
                         .description(" API REST simples de um sistema de notificação hopitalar")
                         .license(new License().name("Apache 2.0").url("https://www.apache.org/licenses/LICENSE-2.0"))
                         .termsOfService("https://ulisses-dev.com.br/pub")
-                        .version("1.0"));
+                        .version("1.0"))
+                .addSecurityItem(new SecurityRequirement().addList("Bearer Authentication"))
+                .components(new Components().addSecuritySchemes("Bearer Authentication", createAPIKeyScheme()));
+    }
+
+    private SecurityScheme createAPIKeyScheme() {
+        return new SecurityScheme()
+                .type(SecurityScheme.Type.HTTP)
+                .bearerFormat("JWT")
+                .scheme("bearer");
     }
 }
